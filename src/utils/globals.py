4a376b089d7a859a5196a89ba4b7b7f528b1111e
Greensky <david.hesliere@gmail.com>
2025-01-18 20:35:40 +0100

    feat: 🧱 Création d'un fichier de variables globales

---
+++ src/utils/globals.py
@@ -0,0 +1,18 @@
+# Variables à utiliser dans tout le projet
+# Les variables sont définies ici sous forme de classe pour éviter les problèmes de portée, et pour avoir l'auto complétion dans les IDE, ainsi que la vérification de type/erreurs pré-exécution
+
+## Variables relatives au joueur
+class movements:
+    left = ["q", "a", "left"]
+    right = ["d", "right"]
+    up = ["z", "w", "up"]
+    down = ["s", "down"]
+
+class player_variables:
+    deplacements_keymap = movements
+
+    dspeed = 3
+
+class ENV:
+    WIDTH = 1600
+    HEIGHT = 1200
