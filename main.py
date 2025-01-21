edc9f9ec1953f1b0b624677c561877ee18099c33
greensky-gs <david.hesliere@gmail.com>
2025-01-21 20:26:05 +0100

    feat: ✨ Menu en classe

--- main.py
+++ main.py
@@ -1,42 +1,20 @@
 from p5 import run, size, background
-from classes.mobs.Player import Player
-from src.utils.globals import player_variables, ENV
-from src.typing.custom_types import *
+from src.utils.globals import ENV
+from src.classes.core.Menu import Menu
 
-player: Player
+menu: Menu
 
 def setup():
-    global player
+    global menu
     background(128)
 
     size(ENV.WIDTH, ENV.HEIGHT)
-    player = Player()
-    player.tp(ENV.WIDTH // 2, ENV.HEIGHT // 2)
+    menu = Menu()
 
 def draw():
     background(128)
-    if player.ready:
-        player.draw()
+    if menu.ready:
+        menu.draw()
 
-    if key_is_pressed:
-        pressed = str(key).lower()
-
-        if pressed in player_variables.deplacements_keymap.left:
-            player.move("negx", 1)
-            moved = True
-        elif pressed in player_variables.deplacements_keymap.right:
-            player.move("posx", 1)
-            moved = True
-        elif pressed in player_variables.deplacements_keymap.up:
-            player.move("negy", 1)
-            moved = True
-        elif pressed in player_variables.deplacements_keymap.down:
-            player.move("posy", 1)
-            moved = True
-
-        if moved:
-            player.drawer.switchState("walk")
-    else:
-        player.drawer.switchState("idle")
 
 run()
