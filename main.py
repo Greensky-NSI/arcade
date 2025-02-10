d0be9c4a9af0682260e7dc25d187b3799aed97d8
Greensky <david.hesliere@gmail.com>
2025-02-10 20:54:52 +0100

    fix: 🚑 Réglage de bug critique

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 from p5 import run, size, background
-from classes.mobs.Player import Player
+from src.classes.mobs.Player import Player
 from src.utils.globals import player_variables, ENV
 from src.typing.custom_types import *
+from src.classes.core.labyrinthe import Labyrinthe, models
 
 player: Player
+laby: Labyrinthe
+initiated = False
+
 
 def setup():
-    global player
+    global player, laby
     background(128)
 
     size(ENV.WIDTH, ENV.HEIGHT)
     player = Player()
-    player.tp(ENV.WIDTH // 2, ENV.HEIGHT // 2)
+    player.tp(5, 5)
+
+    laby = Labyrinthe(ENV.WIDTH, ENV.HEIGHT, 50, model_folder_name="model_one", model_assets_indestructibles_count=2, model_assets_obstacles_count=7)
+    laby.obstacle(models["one"]["obstacles"])
+    laby.indestructible(models["one"]["indestructibles"])
+
+    laby.order()
+
 
 def draw():
-    background(128)
+    global initiated
+
+    if not initiated:
+        background(128)
+
+        laby.afficher_laby()
+        initiated = True
+
     if player.ready:
+        laby.affichage_opti(player.x // laby.taille_case, player.y // laby.taille_case)
         player.draw()
 
     if key_is_pressed:
         pressed = str(key).lower()
 
+        moved = False
         if pressed in player_variables.deplacements_keymap.left:
             player.move("negx", 1)
             moved = True
@@ -38,5 +58,6 @@ def draw():
             player.drawer.switchState("walk")
     else:
         player.drawer.switchState("idle")
+
 
 run()
